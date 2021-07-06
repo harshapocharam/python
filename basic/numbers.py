d4c4b92c4edeb9c6e37a384fe447d86ca05c9f45
Harsha Pocharam <harshanaayini@gmail.com>
2021-07-06 18:12:26 -0400

    num and strings

---
+++ basic/numbers.py
@@ -0,0 +1,14 @@
+print('hello, world!')
+print(1+2)
+print(8*3)
+print()
+print("the end", "or is it?", "keep watching",3)
+#========================================================================
+# literal - having value of some type like int, string, boolean etc
+#function - A name block of code that we can call by using its name. 
+#           we can write our own function or we can use build in python.
+#argument - A value passed to function in order to work with. This may be orguments of 1 or more
+#           arguments appear in parentheses ofter the function name. if no orgumnets we can still have parantheses.
+#function call - using function name we can call a function. when we call a function we have to provide
+#                arguments that call expected.
+#
---
+++ basic/string.py
@@ -0,0 +1,15 @@
+print("today is the good day to learn python")
+print(' we can write in single qotes also')
+print(" we can use 'under double' in python")
+print("hello" + "world")
+greeting = "hello"
+name = input("please enter your name ")
+
+print(greeting + ' ' + name)
+
+# escape sequence
+splitString = " this string has been\n split over \n serveral \n lines"
+print(splitString)
+tabbedString ="1\t2\t3\t4"
+print(tabbedString)
+print(""" we do not need to have escape sequence. """)
